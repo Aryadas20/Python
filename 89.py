7c47abeec0f2abb141e9e2193b3430e03ad2c3b2
Arya bardhan das <dasarya2002@gmail.com>
2023-03-05 20:13:13 +0530

    Update 89.py

--- 89.py
+++ 89.py
@@ -1,14 +1,14 @@
-import requests 
-from bs4 import BeautifulSoup
-url = "https://www.codewithharry.com/blogpost/django-cheatsheet/"
-r = requests.get(url)
-# print(r.text)
+from  import requests 
+# from bs4 import BeautifulSoup
+# url = "https://www.codewithharry.com/blogpost/django-cheatsheet/"
+# r = requests.get(url)
+# # print(r.text)
 
 
-soup = BeautifulSoup(r.text, 'html.parser')
-print(soup.prettify())
-for heading in soup.find_all("h2"):
-  print(heading.text)
+# soup = BeautifulSoup(r.text, 'html.parser')
+# print(soup.prettify())
+# for heading in soup.find_all("h2"):
+#   print(heading.text)
 # url = "https://jsonplaceholder.typicode.com/posts"
 
 # data = {
@@ -22,3 +22,6 @@ for heading in soup.find_all("h2"):
 # response = requests.post(url, headers=headers, json=data)
 
 # print(response.text)
+
+response = requests.get("https://www.amazon.in/?&linkCode=ll2&tag=operagx-desktop-in-21&linkId=f9dfa554c004a3f025d93f062dec5ec6&language=en_IN&ref_=as_li_ss_tl")
+print(response.text)
