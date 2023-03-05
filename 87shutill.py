1159478e825e5784bfdf9b05f67bf18b956128ef
Arya bardhan das <dasarya2002@gmail.com>
2023-03-05 19:51:15 +0530

    Update 87shutill.py

--- 87shutill.py
+++ 87shutill.py
@@ -1,6 +1,8 @@
 import shutil
+import os
 
 # shutil.copy("main.py", "main2.py")
 # shutil.copytree("49file","myfile")
 # shutil.move("49file/file.file", "file.file")
-# shutil.rmtree("")
+
+os.remove("arya.java")
