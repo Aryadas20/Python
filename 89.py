1f01ae7ce4d605b313e11f8295eb1c385edbe242
Arya bardhan das <dasarya2002@gmail.com>
2023-03-05 20:32:30 +0530

    Update 89.py

--- 89.py
+++ 89.py
@@ -1,14 +1,14 @@
 import requests 
 from bs4 import BeautifulSoup
-url = "https://www.codewithharry.com/blogpost/django-cheatsheet/"
-r = requests.get(url)
-# print(r.text)
+# url = "https://www.codewithharry.com/blogpost/django-cheatsheet/"
+# r = requests.get(url)
+# # print(r.text)
 
 
-soup = BeautifulSoup(r.text, 'html.parser')
-print(soup.prettify())
-for heading in soup.find_all("h2"):
-  print(heading.text)
+# soup = BeautifulSoup(r.text, 'html.parser')
+# print(soup.prettify())
+# for heading in soup.find_all("h2"):
+#   print(heading.text)
 # url = "https://jsonplaceholder.typicode.com/posts"
 
 # data = {
@@ -22,3 +22,7 @@ for heading in soup.find_all("h2"):
 # response = requests.post(url, headers=headers, json=data)
 
 # print(response.text) 
+
+
+response = requests.get("https:https://awsacademy.instructure.com/courses/39261/modules")
+print(response.text)
